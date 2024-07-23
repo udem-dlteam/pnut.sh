8f117b3a1cc232d07dc36d6b39fb58efd5aa1cb3
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-07-22 21:33:48 -0400

    Use -O2 and NICE_ERR_MSG options for pnut.js
    -O2 makes the javascript file smaller (no change to WASM file size) and
    NICE_ERR_MSG shows the expected token on unexpected token errors.

--- make-pnut-js.sh
+++ make-pnut-js.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
-emcc ../pnut/pnut.c \
+PNUT_OPTIONS=""
+PNUT_OPTIONS="$PNUT_OPTIONS -Dsh" # shell backend
+PNUT_OPTIONS="$PNUT_OPTIONS -DSUPPORT_INCLUDE" # take input file name as arg
+PNUT_OPTIONS="$PNUT_OPTIONS -DRT_NO_INIT_GLOBALS" # faster script startups
+PNUT_OPTIONS="$PNUT_OPTIONS -DRT_COMPACT" # More compact but slower runtime. Makes for nicer scripts
+PNUT_OPTIONS="$PNUT_OPTIONS -DNICE_ERR_MSG" # Show token information on unexpected token error
+
+
+emcc ../pnut/pnut-lib.c \
+  -O2 \
   -o pnut.js \
-  -Dsh -DSUPPORT_INCLUDE -DRT_NO_INIT_GLOBALS -DEMSCRIPTEN -DRT_COMPACT \
+  $PNUT_OPTIONS \
   -s EXPORTED_FUNCTIONS="['_compile']" \
   -s MODULARIZE \
   -s EXPORT_NAME="create_module" \
   -s EXPORTED_RUNTIME_METHODS="['FS', 'cwrap', 'ExitStatus']" \
-  -s EXIT_RUNTIME=1 \
-  # --preload-file examples/ \
+  -s EXIT_RUNTIME=1
