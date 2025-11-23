0e9abb2a8806402a284142997b5fd42c200452eb
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-11-23 13:02:05 -0500

    Implement compact/fast shell code generation

--- make-pnut-js.sh
+++ make-pnut-js.sh
@@ -2,8 +2,8 @@
 
 set -e -u
 
-PNUT_OPTIONS="-DRELEASE_PNUT_SH"          # shell backend in release mode
-PNUT_OPTIONS="$PNUT_OPTIONS -DRT_COMPACT" # More compact but slower runtime. Makes for nicer scripts
+PNUT_OPTIONS="-Dsh -DNICE_UX -DNO_COLOR -DSAFE_MODE" # shell backend in release mode
+PNUT_OPTIONS_COMPACT="$PNUT_OPTIONS -DRT_COMPACT" # More compact but slower runtime. Makes for nicer scripts
 
 readonly cur_dir=$(pwd)
 
@@ -13,24 +13,31 @@ in_pnut_dir() { # $1 = command
   cd "$cur_dir"  # Go back to the original directory
 }
 
-emcc ../pnut/pnut-lib.c \
-  -O2 \
-  -o pnut.js \
-  $PNUT_OPTIONS \
-  -s EXPORTED_FUNCTIONS="['_compile']" \
-  -s MODULARIZE \
-  -s EXPORT_NAME="create_module" \
-  -s EXPORTED_RUNTIME_METHODS="['FS', 'cwrap', 'ExitStatus']" \
-  -s EXIT_RUNTIME=1
+compile_with_options() { # $1: name, $2: options
+  name="$1"
+  opt="$2"
+  emcc ../pnut/pnut-lib.c \
+    -O2 \
+    -o $name.js \
+    $opt \
+    -s EXPORTED_FUNCTIONS="['_compile']" \
+    -s MODULARIZE \
+    -s EXPORT_NAME="create_${name}_module" \
+    -s EXPORTED_RUNTIME_METHODS="['FS', 'cwrap', 'ExitStatus']" \
+    -s EXIT_RUNTIME=1
 
-# Add release information to the generated pnut.js file
+  # Add release information to the generated pnut.js file
 
-COMMIT_HASH=$(in_pnut_dir "git rev-parse HEAD")
-# Date in DAY/MONTH/YEAR format
-CUR_DATE=$(date +'%d/%m/%Y')
-printf \
-"\
-// Date $CUR_DATE
-// Pnut commit hash = '$COMMIT_HASH';
-// PNUT_OPTIONS = '$PNUT_OPTIONS';
-" | cat - pnut.js > temp && mv temp pnut.js
+  COMMIT_HASH=$(in_pnut_dir "git rev-parse HEAD")
+  # Date in DAY/MONTH/YEAR format
+  CUR_DATE=$(date +'%d/%m/%Y')
+  printf \
+  "\
+  // Date $CUR_DATE
+  // Pnut commit hash = '$COMMIT_HASH';
+  // PNUT_OPTIONS = '$opt';
+  " | cat - $name.js > temp && mv temp $name.js
+}
+
+compile_with_options "pnut_standard" "$PNUT_OPTIONS"
+compile_with_options "pnut_compact" "$PNUT_OPTIONS_COMPACT"
